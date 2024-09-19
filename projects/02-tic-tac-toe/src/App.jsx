9a320cc6e0763c39d2ddb69e4ea3e1417e85977e
cesarmilan16 <cesar3bfermi@gmail.com>
2024-09-19 20:11:38 +0200

    35'

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,17 +5,79 @@ const TURNS = {
   O: "o"
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  // Filas
+  [0, 1, 2], // Primera fila
+  [3, 4, 5], // Segunda fila
+  [6, 7, 8], // Tercera fila
+  
+  // Columnas
+  [0, 3, 6], // Primera columna
+  [1, 4, 7], // Segunda columna
+  [2, 5, 8], // Tercera columna
+  
+  // Diagonales
+  [0, 4, 8], // Diagonal principal
+  [2, 4, 6]  // Diagonal secundaria
+];
+
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador y false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // Revisamos todas las combinaciones ganadoras
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // Si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // No actualizamos esta posición
+    // si ya tiene algo
+    if (board[index] || winner) return
+    // Actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn // X u O
+    setBoard(newBoard)
+    // Cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner) // Esto es asincrono
+      alert('Ganó ' + newWinner)
+    }
+  }
 
   return (
     <main className="board">
@@ -27,12 +89,22 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
